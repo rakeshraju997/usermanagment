b7407a707e9eb7f983f6e418c1baeff6d5b61388
Rakesh <rakeshraju997@gmail.com>
2022-09-01 13:49:48 +0530

    add user updated

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -16,17 +16,17 @@ const pool = mysql.createPool({
 exports.adminDash = (req, res) => {
     session = req.session;
     //connect to DB
-    pool.getConnection((err, connection)=>{
-        if(err) throw err; // not connected
-        console.log('Connected as ID '+ connection.threadId);
+    pool.getConnection((err, connection) => {
+        if (err) throw err; // not connected
+        console.log('Connected as ID ' + connection.threadId);
 
         //User the connection
-        connection.query('SELECT * FROM users WHERE id = ?',[session.userid],(err, rows, field)=>{
+        connection.query('SELECT * FROM users WHERE id = ?', [session.userid], (err, rows, field) => {
 
             //when done with the connection,release it
             connection.release();
-            if(!err && rows.length>0){
-                res.render('admin', { loginPage: true , rows});
+            if (!err && rows.length > 0) {
+                res.render('admin', { loginPage: true, rows });
             }
 
         });
@@ -38,30 +38,30 @@ exports.adminDash = (req, res) => {
 exports.userList = (req, res) => {
     session = req.session;
     // connect to DB
-    pool.getConnection((err, connection)=>{
-        if(err) throw err; // not connected
-        console.log('Connected as ID '+ connection.threadId);
+    pool.getConnection((err, connection) => {
+        if (err) throw err; // not connected
+        console.log('Connected as ID ' + connection.threadId);
 
         //User the connection
-        connection.query('SELECT * FROM users WHERE id = ?',[session.userid],(err, rows, field)=>{
+        connection.query('SELECT * FROM users WHERE id = ?', [session.userid], (err, rows, field) => {
 
             //when done with the connection,release it
-            
-            if(!err && rows.length>0){
-                connection.query('SELECT * FROM users ORDER BY id',(err, usersRow, field)=>{
-                    console.log(usersRow);
-                    res.render('userlist', { loginPage: true, rows, usersRow});
-                    
+
+            if (!err && rows.length > 0) {
+                connection.query('SELECT * FROM users ORDER BY id', (err, usersRow, field) => {
+                    res.render('userlist', { loginPage: true, rows, usersRow });
+
                 });
 
                 connection.release();
-                
+
             }
 
         });
     });
 }
 
+// Display adduser form
 exports.adduserform = (req, res) => {
     session = req.session;
     // connect to DB
@@ -82,24 +82,48 @@ exports.adduserform = (req, res) => {
     });
 }
 
+// Add user
 exports.adduser = (req, res) => {
-    const {firstname, lastname, email, role, password, cpassword} = req.body;
-    console.log(firstname)
+    session = req.session;
+    const { firstname, lastname, email, role, password, cpassword } = req.body;
+    if (password != cpassword) {
+            loginedUser(session, function(rows){
+                res.render('adduser', { loginPage: true, alertcolor : 'danger', alert: 'Password not matching', rows });
+            });
+    } else {
+        // connect to DB
+        pool.getConnection((err, connection) => {
+            if (err) throw err; // not connected
+            console.log('Connected as ID ' + connection.threadId);
+
+            //User the connection
+            connection.query('INSERT INTO users SET first_name = ?, last_name = ?, email = ?, password = ?, role = ?', [firstname, lastname, email, password, role], (err, row) => {
+
+                //when done with the connection,release it
+                connection.release();
+                loginedUser(session, function(rows){
+                    if (!err) {
+                        res.render('adduser', { loginPage: true, alertcolor: 'success', alert: 'User added succefully.', rows });
+                    } else {
+                        res.render('adduser', { loginPage: true, alertcolor: 'danger', alert: 'Check your details.', rows });
+                    }
+                });
+            });
+        });
+    }
+}
+
+function loginedUser(session, callback){
     // connect to DB
     pool.getConnection((err, connection) => {
         if (err) throw err; // not connected
         console.log('Connected as ID ' + connection.threadId);
-
         //User the connection
-        connection.query('INSERT INTO users SET first_name = ?, last_name = ?, email = ?, password = ?, role = ?',[firstname, lastname, email, password, role], (err, rows) => {
-
+        connection.query('SELECT * FROM users WHERE id = ?',[session.userid], (err, rows, field) => {
             //when done with the connection,release it
             connection.release();
-
-            if (!err) {
-                res.render('adduser', { loginPage: true, alert:'User added succefully.' });
-            }else{
-                res.render('adduser', { loginPage: true, alert:'Check your details.' });
+            if (!err && rows.length > 0) {
+                callback(rows);
             }
         });
     });
